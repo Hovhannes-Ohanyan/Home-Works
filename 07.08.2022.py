1469b544ecaf0472088de359416c887aef6b831b
Hovhannes-Ohanyan <108061081+Hovhannes-Ohanyan@users.noreply.github.com>
2022-08-09 14:13:52 +0400

    Add files via upload

---
+++ 07.08.2022.py
@@ -0,0 +1,45 @@
+#3
+# def SumOf100():
+#     result = []
+#     for i in range(1, 101):
+#         result.append(i)
+#     return result
+#
+#
+# def squareSum():
+#     result = 0
+#     res = []
+#     for i in range(1, 101):
+#         res.append(pow(i, 2))
+#         result = sum(res)
+#     return result
+#
+#
+# def patasxan():
+#     return sum(SumOf100()) - squareSum()
+#
+#
+# print(patasxan())
+
+#4
+
+# f=open("text.txt")
+# f1=open("result.txt","a")
+# for line in f.readlines():
+#     if line.isupper():
+#         line.islower()
+#         f1.write(line.title())
+# f.close()
+# f1.close()
+# print(f1)
+
+#5
+# def palindrom():
+#     number=input("write number:")
+#     if number==number[::-1]:
+#         return True
+#     else:
+#         return False
+# print(palindrom())
+
+
